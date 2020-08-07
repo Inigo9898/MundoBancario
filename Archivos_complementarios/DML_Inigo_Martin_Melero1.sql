ff1bcff13ab59ccec1a2b4430206cba4f6a767ec
Inigo9898 <66134874+Inigo9898@users.noreply.github.com>
2020-08-07 18:03:14 +0200

    Delete DML_Inigo_Martin_Melero1.sql

--- Archivos_complementarios/DML_Inigo_Martin_Melero1.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO mundobancario.tipos_movimiento (tipo) VALUES ('INGRESO');
-INSERT INTO mundobancario.tipos_movimiento (tipo) VALUES ('PRESTAMO');
-INSERT INTO mundobancario.tipos_movimiento (tipo) VALUES ('PAGO');
-INSERT INTO mundobancario.tipos_movimiento (tipo) VALUES ('AMORTIZACION');
-INSERT INTO mundobancario.tipos_movimiento (tipo) VALUES ('INTERESES');
